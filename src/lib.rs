e74fcbba7ac09267319df44ed485f21ecf5490ed
Ukuma012 <Ukuma012@gmail.com>
2024-02-23 23:18:13 +0900

    unimplemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
-extern crate dotenv;
 extern crate log;
 
 #[macro_use]
 extern crate diesel;
 extern crate r2d2;
 
-pub fn run(listner: TcpListener, db_name: &str) -> Result<Server, std::io::Error> {}
+pub fn run(listner: TcpListener, db_name: &str) -> Result<Server, std::io::Error> {
+    unimplemented!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use dotenvy;
 use std::env;
 use std::net::TcpListener;
 
@@ -5,17 +6,16 @@ use test_api::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let environment_file;
-    if let Ok(e) = env::var("ENV") {
-        environment_file = format!(".env.{}", e);
-    } else {
-        environment_file = String::from(".env");
-    }
+    dotenvy::dotenv().ok();
+    let environment = match env::var("APP_ENVIRONMENT") {
+        Ok(val) => val,
+        Err(_) => "development".to_string(),
+    };
 
-    dotenv::from_filename(environment_file).ok();
+    dotenvy::from_filename(".env.".to_string() + &environment).ok();
 
     let listener = TcpListener::bind("0.0.0.0:8888").expect("Failed to bind random port");
-    let database_name = dotenv::var("DATABASE_NAME").expect("DATABASE_NAME must be set");
+    let database_name = env::var("DATABASE_NAME").expect("DATABASE_NAME must be set");
 
-    run(listener, &database_name)?.await 
+    run(listener, &database_name)?.await
 }
