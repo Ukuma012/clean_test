76d25a891f1acdfb50a931394fcd7ba2f744270d
Ukuma012 <Ukuma012@gmail.com>
2024-02-26 10:05:44 +0900

    route to invitation

--- src/adapters/api/shared/routes.rs
+++ src/adapters/api/shared/routes.rs
@@ -1,6 +1,8 @@
-use crate::adapters::api::healthcheck::healthcheck_controllers;
+use crate::adapters::api::{healthcheck::healthcheck_controllers, invitation::invitation_controller};
 use actix_web::web;
 
 pub fn routes(config: &mut web::ServiceConfig) {
-    config.service(web::scope("/api/v1/healthcheck").configure(healthcheck_controllers::routes));
+    config
+        .service(web::scope("/api/v1/healthcheck").configure(healthcheck_controllers::routes))
+        .service(web::scope("/api/v1/register/invitation").configure(invitation_controller::routes));
 }
