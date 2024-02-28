8769b8615248c9c2e8a85516a37ab1b7121e0324
Ukuma012 <Ukuma012@gmail.com>
2024-02-28 17:38:00 +0900

    mock test

--- src/application/repositories/email_repository_abstract.rs
+++ src/application/repositories/email_repository_abstract.rs
@@ -1,7 +1,10 @@
-use std::error::Error;
-
 use async_trait::async_trait;
 
+#[cfg(test)]
+use mockall::{predicate::*, *};
+use std::error::Error;
+
+#[cfg_attr(test, automock)]
 #[async_trait(?Send)]
 pub trait EmailRepositoryAbstract {
     fn send_email(&self, recipient: &str, subject: &str, body: String) -> Result<(), Box<dyn Error>>;
--- src/application/repositories/invitation_repository_abstract.rs
+++ src/application/repositories/invitation_repository_abstract.rs
@@ -1,8 +1,12 @@
 use async_trait::async_trait;
 
 use crate::domain::invitation_entity::InvitationEntity;
+
+#[cfg(test)]
+use mockall::{predicate::*, *};
 use std::error::Error;
 
+#[cfg_attr(test, automock)]
 #[async_trait(?Send)]
 pub trait InvitationRepositoryAbstract {
     async fn insert_invitation(&self, email: String) -> Result<InvitationEntity, Box<dyn Error>>;
